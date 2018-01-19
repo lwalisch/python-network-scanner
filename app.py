caccf6562a7de1ac5a5cf500a684c5b666679507
Lukas Walisch <lukas.walisch@gmail.com>
2018-01-19 10:31:06 +0100

    adding progress bar

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import subprocess
 import re
 
+from progress import printProgressBar
+
 INVALID_IP_ADRESSES = ["127.0.0.1", "172.17.0.1"]
 
+
 def app() :
-
-
 
     # execute "ifconfig" in shell
     shell = subprocess.Popen(["ifconfig"], stdout=subprocess.PIPE)
@@ -19,6 +20,8 @@ def app() :
 
     # create a subnet mask according to the valid ips and try to ping each host in the subnet
     network_hosts = []
+    counter = 0
+    printProgressBar(counter, 50, prefix = 'Progress:', suffix = 'Complete', length = 50)
     for local_ip in local_ips:
         subnet_mask = re.match(r'(?:[0-9]{1,3}\.){3}', local_ip).group(0)
         for i in range(1, 255):
@@ -26,26 +29,16 @@ def app() :
             shell = subprocess.Popen(["ping", ip, "-c", "1"], stdout=subprocess.PIPE)
 
             try:
-                ping_result = shell.communicate(timeout=0.1)[0].decode("utf-8")
+                ping_result = shell.communicate(timeout=0.05)
                 network_hosts.append(ip)
-            except:
+            except subprocess.TimeoutExpired:
                 pass
-            print(".")
+
+            if i % 5 == 0:
+                counter += 1
+                printProgressBar(counter, 50, prefix = 'Scanning Network:', length = 50)
 
         print(network_hosts)
 
-        # for i in range(1,255):
-        #     ip = subnet_mask + i
-        #     shell = subprocess.Popen([ip, "-c", "1"])
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     app()
-
-
